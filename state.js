adb0851e744fe21d89f4bcc72ddbe0ceb172dd0a
youngjerald239 <youngjerald239@gmail.com>
2022-09-03 13:12:35 -0500

    created running state

--- state.js
+++ state.js
@@ -3,6 +3,8 @@ export const states = {
     STANDING_RIGHT: 1,
     SITTING_LEFT: 2,
     SITTING_RIGHT: 3,
+    RUNNING_LEFT: 4, 
+    RUNNING_RIGHT: 5,
 }
 
 class State {
@@ -21,7 +23,8 @@ export class StandingLeft extends State {
         this.player.frameY = 1
     }
     handleInput(input){
-        if (input === 'PRESS right') this.player.setState(states.STANDING_RIGHT) // set state to StandingRight 
+        if (input === 'PRESS right') this.player.setState(states.RUNNING_RIGHT) // set state to StandingRight 
+        if (input === 'PRESS left') this.player.setState(states.RUNNING_LEFT)
         else if (input === 'PRESS down') this.player.setState(states.SITTING_LEFT)
     }
 }
@@ -35,7 +38,8 @@ export class StandingRight extends State {
         this.player.frameY = 0
     }
     handleInput(input){
-        if (input === 'PRESS left') this.player.setState(states.STANDING_LEFT)// set state to StandingLeft
+        if (input === 'PRESS left') this.player.setState(states.RUNNING_LEFT)// set state to StandingLeft
+        else if (input === 'PRESS right') this.player.setState(states.RUNNING_RIGHT)
         else if (input === 'PRESS down') this.player.setState(states.SITTING_RIGHT) 
     }
 }
@@ -50,7 +54,7 @@ export class SittingLeft extends State {
     }
     handleInput(input){
         if (input === 'PRESS right') this.player.setState(states.SITTING_RIGHT) // set state to StandingRight 
-        else if(input === 'PRESS up') this.player.setState(states.STANDING_LEFT)
+        else if(input === 'RELEASE down') this.player.setState(states.STANDING_LEFT)
     }
 }
 export class SittingRight extends State {
@@ -64,6 +68,36 @@ export class SittingRight extends State {
     }
     handleInput(input){
         if (input === 'PRESS left') this.player.setState(states.SITTING_LEFT) // set state to StandingRight 
-        else if (input === 'PRESS up') this.player.setState(states.STANDING_RIGHT)
+        else if(input === 'RELEASE down') this.player.setState(states.STANDING_RIGHT)
+    }
+}
+export class RunningLeft extends State {
+    constructor(player) {
+        super('RUNNING LEFT')
+        this.player = player
+
+    }
+    enter(){
+        this.player.frameY = 7
+    }
+    handleInput(input){
+        if (input === 'PRESS right') this.player.setState(states.RUNNING_RIGHT) // set state to StandingRight 
+        else if(input === 'RELEASE left') this.player.setState(states.STANDING_LEFT)
+        else if (input === 'PRESS down') this.player.setState(states.SITTING_LEFT)
+    }
+}
+export class RunningRight extends State {
+    constructor(player) {
+        super('RUNNING RIGHT')
+        this.player = player
+
+    }
+    enter(){
+        this.player.frameY = 6
+    }
+    handleInput(input){
+        if (input === 'PRESS left') this.player.setState(states.RUNNING_LEFT) // set state to StandingRight 
+        else if(input === 'RELEASE right') this.player.setState(states.STANDING_RIGHT)
+        else if (input === 'PRESS down') this.player.setState(states.SITTING_RIGHT)
     }
 }
