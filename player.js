662621aa7a88b0538263d059643634fa2feab3bf
youngjerald239 <youngjerald239@gmail.com>
2022-09-03 10:25:35 -0500

    draw player on canvas

--- player.js
+++ player.js
@@ -9,10 +9,12 @@ export default class Player {
         this.height = 181.83
         this.x = 0
         this.y = 0
+        this.frameX = 0
+        this.frameY = 0
 
     }
     draw(context){
-        context.drawImage(this.image, this.x, this.y)
+        context.drawImage(this.image, this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height)
 
     }
 }
